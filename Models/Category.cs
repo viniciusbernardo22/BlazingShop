947bf13d91b41eac25de0ce10aecc8a99ce81195
Vinicius <vini383@gmail.com>
2024-01-26 15:43:10 -0300

    feat: added login component

---
+++ Models/Category.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazingShop.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+        }
+
+        public Category(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,22 @@
+﻿namespace BlazingShop.Models
+{
+    public class Product
+    {
+        public Product(int id, string name, string description, string image, decimal price, int categoryId)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Image = image;
+            Price = price;
+            CategoryId = categoryId;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
